5400b8dcfdd79d94d431debcef5a28b62755672d
itwister <itwisterlx@gmail.com>
2015-09-07 16:46:36 +0400

    настройка проекта,  смена формат URL запросов

--- RemoteCache.Web/Controllers/CacheController.cs
+++ RemoteCache.Web/Controllers/CacheController.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.AspNet.Mvc;
 using RemoteCache.Web.Models;
 using System;
-using System.Net;
 using System.IO;
+using System.Net;
 
 namespace RemoteCache.Web.Controllers
 {
+    [Route("[controller]")]
     public class CacheController : Controller
     {
         RemoteImageRepository imageRepository = new RemoteImageRepository();
         BaseImageResizer resizer = new DefaultImageResizer();
 
-        public ActionResult Get(string url, string format, string bgColor, int? width = null)
+        [Route("original")]
+        public ActionResult Original(string url, string format)
+        {
+            return DoExecute(url, format, null, null);
+        }
+
+        [Route("fitWidth")]
+        public ActionResult FitWidth(string url, int width, string format, string bgColor)
+        {
+            return DoExecute(url, format, bgColor, width);
+        }
+
+        ActionResult DoExecute(string url, string format, string bgColor, int? width)
         {
             Uri tmp;
             if (!Uri.TryCreate(url, UriKind.Absolute, out tmp))
@@ -43,11 +56,11 @@ namespace RemoteCache.Web.Controllers
 
         void ConfigureCache()
         {
-//  #if !DEBUG
-//              var cache = Response.Cache;
-//              cache.SetCacheability(HttpCacheability.Public);
-//              cache.SetExpires(new DateTime(2525, 1, 1));
-//  #endif
+            //  #if !DEBUG
+            //              var cache = Response.Cache;
+            //              cache.SetCacheability(HttpCacheability.Public);
+            //              cache.SetExpires(new DateTime(2525, 1, 1));
+            //  #endif
         }
     }
 }
