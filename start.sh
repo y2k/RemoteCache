07426951933e166c6924cd12d84d67288321a90c
y2k <itwisterlx@gmail.com>
2015-12-12 09:59:48 +0400

    fix ffmpeg path

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ killall mono
 source $HOME/.dnx/dnvm/dnvm.sh
 dnvm use -r mono 1.0.0-rc1-update1
 
-export REMOTECACHE_FFMPEG_DIR=/url/bin
+export REMOTECACHE_FFMPEG_DIR=/usr/bin
 
 cd RemoteCache.Worker
 nohup dnx run &
