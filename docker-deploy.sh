d217fe719f5a30d73899b2e70266ed22b5a29111
y2k <itwisterlx@gmail.com>
2015-12-27 20:11:46 +0000

    ssl

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -6,4 +6,4 @@ docker rmi -f "remote-cache"
 docker build -t "remote-cache" .
 
 cd ..
-docker run -v $PWD/ssl:/etc/nginx/ssl -v /tmp/remote-cache:/app/Cache --name "remote-cache" -d -p 8010-8012:8010-8012 "remote-cache"
+docker run -v $1:/etc/nginx/ssl -v /tmp/remote-cache:/app/Cache --name "remote-cache" -d -p 8010-8012:8010-8012 "remote-cache"
