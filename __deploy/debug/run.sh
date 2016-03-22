5f075c01643dc72faa74e2b04f7676c3a564e325
y2k <itwisterlx@gmail.com>
2016-03-22 16:28:13 +0400

    refactor deploy script

---
+++ __deploy/debug/run.sh
@@ -0,0 +1,4 @@
+"#!/bin/bash
+
+dnx -p RemoteCache.Worker/project.json run &
+dnx -p RemoteCache.Web/project.json web &
---
+++ docker-deploy.sh
@@ -0,0 +1,25 @@
+if [ "$#" != "2" ]; then
+	if [ "$#" != "3" ]; then
+		echo "docker-deploy.sh <debug|production> <cache-dir> <hostname>"
+		exit 1
+	fi
+fi
+
+docker stop "remote-cache"
+docker rm -f "remote-cache"
+docker rmi -f "remote-cache"
+
+rm -f Dockerfile
+rm -f nginx.conf
+rm -f run.sh
+
+if [ "$1" != "debug" ]; then
+	cp __deploy/debug/Dockerfile .
+	cp __deploy/debug/run.sh .
+
+	docker build -t "remote-cache" .
+	docker run -v $(realpath $2):/app/Cache --name "remote-cache" -d -p 8080:8011 "remote-cache"
+elif [ "$1" != "production" ]; then
+	docker build --build-arg SSL_DIR=/etc/letsencrypt/live/$3 -t "remote-cache" .
+	docker run -v /etc/letsencrypt:/etc/letsencrypt -v $(realpath $2):/app/Cache --name "remote-cache" -d -p 8081:8011 --restart on-failure "remote-cache"
+fi
