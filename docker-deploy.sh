cc804204b0e530c1ffb009223726af68c27fce1f
y2k <itwisterlx@gmail.com>
2016-03-29 09:55:52 +0400

    cloud flare

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -28,7 +28,7 @@ elif [ "$1" == "production" ]; then
 	cp __deploy/production/nginx.conf .
 
 	docker build --build-arg SSL_DIR=/etc/letsencrypt/live/$HOSTNAME -t "remote-cache" .
-	docker run -v /etc/letsencrypt:/etc/letsencrypt -v $CACHE_PATH:/app/RemoteCache.Worker/Cache --name "remote-cache" -d -p 8011-8012:8081-8082 --restart on-failure "remote-cache"
+	docker run -v /etc/letsencrypt:/etc/letsencrypt -v $CACHE_PATH:/app/RemoteCache.Worker/Cache --name "remote-cache" -d -p 8011:8081 -p 8080:8082 --restart on-failure "remote-cache"
 fi
 
 rm -f Dockerfile
